1ed950cb1a158ef47c0d6fe0b47035f0274ebab2
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-05-09 12:36:15 +0600

    initial setup

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,10 @@
+
+const AddCoffee = () => {
+    return (
+        <div>
+            add a Coffee
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,10 @@
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            update a Coffee
+        </div>
+    );
+};
+
+export default UpdateCoffee;
